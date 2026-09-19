8f242e0e9f4292c98ff7488efb9de19f5c539f4a
Phillip Mienk <mienkphi@gmail.com>
2026-09-19 01:25:25 -0700

    Regenerate artifacts.

--- include/bitcoin/explorer.hpp
+++ include/bitcoin/explorer.hpp
@@ -24,20 +24,6 @@
 #include <bitcoin/explorer/parser.hpp>
 #include <bitcoin/explorer/utility.hpp>
 #include <bitcoin/explorer/version.hpp>
-#include <bitcoin/explorer/config/address.hpp>
-#include <bitcoin/explorer/config/algorithm.hpp>
-#include <bitcoin/explorer/config/btc.hpp>
-#include <bitcoin/explorer/config/byte.hpp>
-#include <bitcoin/explorer/config/bytes.hpp>
-#include <bitcoin/explorer/config/ec_private.hpp>
-#include <bitcoin/explorer/config/electrum.hpp>
-#include <bitcoin/explorer/config/endorsement.hpp>
-#include <bitcoin/explorer/config/hd_key.hpp>
-#include <bitcoin/explorer/config/language.hpp>
-#include <bitcoin/explorer/config/sighash.hpp>
-#include <bitcoin/explorer/config/signature.hpp>
-#include <bitcoin/explorer/config/witness.hpp>
-#include <bitcoin/explorer/config/wrapper.hpp>
 #include <bitcoin/explorer/commands/address-decode.hpp>
 #include <bitcoin/explorer/commands/address-embed.hpp>
 #include <bitcoin/explorer/commands/address-encode.hpp>
@@ -154,5 +140,19 @@
 #include <bitcoin/explorer/commands/witness-to-key.hpp>
 #include <bitcoin/explorer/commands/wrap-decode.hpp>
 #include <bitcoin/explorer/commands/wrap-encode.hpp>
+#include <bitcoin/explorer/config/address.hpp>
+#include <bitcoin/explorer/config/algorithm.hpp>
+#include <bitcoin/explorer/config/btc.hpp>
+#include <bitcoin/explorer/config/byte.hpp>
+#include <bitcoin/explorer/config/bytes.hpp>
+#include <bitcoin/explorer/config/ec_private.hpp>
+#include <bitcoin/explorer/config/electrum.hpp>
+#include <bitcoin/explorer/config/endorsement.hpp>
+#include <bitcoin/explorer/config/hd_key.hpp>
+#include <bitcoin/explorer/config/language.hpp>
+#include <bitcoin/explorer/config/sighash.hpp>
+#include <bitcoin/explorer/config/signature.hpp>
+#include <bitcoin/explorer/config/witness.hpp>
+#include <bitcoin/explorer/config/wrapper.hpp>
 
 #endif
